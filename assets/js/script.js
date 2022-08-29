934e1bf38674ab590e10267d0c3700be5dee302d
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-08-29 13:39:36 -0600

    was able to populate the forcast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,10 @@ var tempEl= document.getElementById("temp");
 var windEl= document.getElementById("wind");
 var humidityEl= document.getElementById("humidity");
 var uvEl= document.getElementById("UV");
-var forcastTemp = document.getElementById('card-temp');
-var forcastWind = document.getElementById('card-wind');
-var forcastHumidity = document.getElementById('card-humidity');
-var cardTitle = document.getElementById("card-title");
+var forcastDiv = document.getElementById("forcast");
 var weatherDashboard = document.getElementById('cityname');
-var card1 = document.getElementById('card-1');
-var card2 = document.getElementById('card-2');
-var card3 = document.getElementById('card-3');
-var card4 = document.getElementById('card-4');
-var card5 = document.getElementById('card-5');
-let date = new Date().toUTCString().slice(5, 16);
-var forcastDays = [0,1,2,3,4,5]
+var date = new Date().toUTCString().slice(5, 16);
+
 
 function weatherFunction(city){
     var apiParams = new URLSearchParams({
@@ -53,21 +45,6 @@ function weatherFunction(city){
         }
 
 
-    // create header in dashboard with the city input and todays date
-
-
-    // create text content for each value of temp, wind, humidity, UV in the dashboard
-
-    // dynamically create cards with a header of the next days date date and temp wind humidity
-
-
-
-
-
-
-
-
-
 
     var forcastweatherAPI = 
     "https://api.weatherbit.io/v2.0/forecast/daily?"+apiParams;
@@ -83,26 +60,25 @@ function weatherFunction(city){
     });
 
     var forcastWeather = function(forcast){
-        for (let i = 0; i < forcastDays.length; i++) {
-            
+        for (let i = 1; i < 6; i++) {
 
 
-
-
-
-
-
-
-        // cardTitle.textContent="Forcast: " + forcast.data[1].valid_date;
+            var cardTitle = document.getElementById("date-"+[i]);
+            var forcastTemp=document.getElementById("temp-"+[i]);
+            var forcastWind= document.getElementById("wind-" + [i]);
+            var forcastHumidity = document.getElementById("humidity-" + [i]);
+                    
+            cardTitle.textContent="Forcast: " + forcast.data[i].valid_date;
+                    
             forcastTemp.textContent="Temperature: " + forcast.data[i].temp + '°F';
+                    
             forcastWind.textContent="Wind: " + forcast.data[i].wind_spd + "MPH";
+                    
             forcastHumidity.textContent="Humidity: " +forcast.data[i].rh + "%";
+                    
+                    }
 
-
-        }
     }
-
-
 }
 searchBtn.addEventListener('click', function(){
     var city = cityInput.value;
